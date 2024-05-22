fc287ab1a8e67c7bafdd21e513fafb2b81cf6e32
Cenate <valeriecenate@northeastern.edu>
2024-05-22 09:05:14 -0400

    first commit

---
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -0,0 +1,115 @@
+export default function AssignmentEditor() {
+    return (
+      <div id="wd-assignments-editor">
+        <label htmlFor="wd-name">Assignment Name</label> <br /><br />
+        <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
+        <textarea id="wd-description" cols={40} rows={10}>
+          The assignment is available online Submit a link to the landing page of your Web applicatin running on Netlify.  The landing page should include the following: Your full name and section links to each of the lab assignments.  Link to the Kanbas application.  Links to all relevant source code repositories.  The kanbas application should include a link to navigate back tothe landing page.
+        </textarea>
+        <br /><br />
+        <table>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-points">Points</label>
+            </td>
+            <td>
+              <input id="wd-points" value={100} />
+            </td>
+          </tr>
+          <br />
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-group">Assignment Group</label>
+            </td>
+            <td>
+            <select id="wd-group">
+            <option selected value="Assignments">Assignments</option>
+            <option value="Assignment1">Assignment 1</option>
+            <option value="Assignment2">
+                Assignment 2</option>
+            </select>
+            </td>
+          </tr>
+          <br />
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-display-grade-as">Display Grade as</label>
+            </td>
+            <td>
+            <select id="wd-display-grade-as">
+            <option selected value="Percentage">Percentage</option>
+            <option value="Letter">Letter Grade</option>
+            </select>
+            </td>
+          </tr>
+          <br />
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-submission-type">Submission Type</label>
+            </td>
+            <td>
+            <select id="wd-submission-type">
+            <option selected value="Online">Online</option>
+            <option value="InPerson">In Person</option>
+            </select>
+            </td>
+          </tr>
+          <br />
+          <tr>
+            <td></td>
+            <td>
+            <label>Online Entry Options</label><br/>
+
+            <input type="checkbox" name="check-genre" id="wd-text-entry"/>
+            <label htmlFor="wd-text-entry">Text Entry</label><br/>
+
+            <input type="checkbox" name="check-genre" id="wd-website-url"/>
+            <label htmlFor="wd-website-url">Website URL</label><br/>
+
+            <input type="checkbox" name="check-genre" id="wd-media-recording"/>
+            <label htmlFor="wd-media-recordings">Media Recordings</label><br/>
+
+            <input type="checkbox" name="check-genre" id="wd-student-annotation"/>
+            <label htmlFor="wd-student-annotation">Student Annotatioin</label><br/>
+
+            <input type="checkbox" name="check-genre" id="wd-file-upload"/>
+            <label htmlFor="wd-file-upload">File Uploads</label>
+            </td>
+          </tr>
+          <br />
+          <tr>
+            <td></td>
+            <td>
+            <label htmlFor="wd-assign-to">Assign To</label><br />
+              <input id="wd-assign-to" value={"Everyone"} />
+            </td>
+          </tr>
+          <br />
+          <tr>
+            <td></td>
+            <td>
+            <label htmlFor="wd-due-date">Due </label><br />
+            <input type = "date" id="wd-due-date" value ="2024-04-13" />
+            </td>
+          </tr>
+          <br />
+          <tr>
+            <td></td>
+          <td>
+            <label htmlFor="wd-available-from">Available From </label><br />
+            <input type = "date" id="wd-available-from" value ="2024-04-13" />
+            </td>
+            <td>
+            <label htmlFor="wd-available-until">Until </label><br />
+            <input type = "date" id="wd-available-until" value ="2024-04-13" />
+            </td><hr />
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td><br /><button>Cancel</button> <button>Save</button></td>
+          </tr>
+        </table>
+      </div>
+  );}
+  
---
+++ src/Kanbas/Dashboard/index.tsx
@@ -0,0 +1,146 @@
+export default function Dashboard() {
+    return (
+      <div id="wd-dashboard">
+        <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+        <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+      
+        <div id="wd-dashboard-courses">
+        <table>
+            <tr>
+                <td>
+                <div className="wd-dashboard-course">
+                <img src="/images/reactjs.jpg" width={200} />
+                <div>
+                <a className="wd-dashboard-course-link"
+                    href="#/Kanbas/Courses/1234/Home">
+                    CS1234 React JS
+                </a>
+                <p className="wd-dashboard-course-title">
+                    Full Stack software developer
+                </p>
+                <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+                </div>
+                 </div>
+                </td>
+                <td>
+                <div className="wd-dashboard-course"> 
+                <img src="/images/dog.jpg" width={200} />
+                <div>
+                    <a className="wd-dashboard-course-link"
+                        href="#/Kanbas/Courses/1234/Home">
+                        Dog Grooming 101
+                    </a>
+                    <p className="wd-dashboard-course-title">
+                    How to Groom Your Pet at Home
+                    </p>
+                    <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+                    </div>
+                </div>
+                </td>
+                <td>
+                <div className="wd-dashboard-course"> 
+                <img src="/images/dog.jpg" width={200} />
+                <div>
+                    <a className="wd-dashboard-course-link"
+                        href="#/Kanbas/Courses/1234/Home">
+                        Dog Grooming 102
+                    </a>
+                    <p className="wd-dashboard-course-title">
+                    How to Groom Your Pet at Home
+                    </p>
+                    <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+                    </div>
+                </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                <div className="wd-dashboard-course"> 
+                <img src="/images/dog.jpg" width={200} />
+                <div>
+                    <a className="wd-dashboard-course-link"
+                        href="#/Kanbas/Courses/1234/Home">
+                        Dog Grooming 103
+                    </a>
+                    <p className="wd-dashboard-course-title">
+                    How to Groom Your Pet at Home
+                    </p>
+                    <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+                    </div>
+                </div>
+                </td>
+                <td>
+                <div className="wd-dashboard-course"> 
+                <img src="/images/dog.jpg" width={200} />
+                <div>
+                    <a className="wd-dashboard-course-link"
+                        href="#/Kanbas/Courses/1234/Home">
+                        Dog Grooming 104
+                    </a>
+                    <p className="wd-dashboard-course-title">
+                    How to Groom Your Pet at Home
+                    </p>
+                    <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+                    </div>
+                </div>
+                </td>
+                <td>
+                <div className="wd-dashboard-course"> 
+                <img src="/images/dog.jpg" width={200} />
+                <div>
+                    <a className="wd-dashboard-course-link"
+                        href="#/Kanbas/Courses/1234/Home">
+                        Dog Grooming 105
+                    </a>
+                    <p className="wd-dashboard-course-title">
+                    How to Groom Your Pet at Home
+                    </p>
+                    <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+                    </div>
+                </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                <div className="wd-dashboard-course"> 
+                <img src="/images/dog.jpg" width={200} />
+                <div>
+                    <a className="wd-dashboard-course-link"
+                        href="#/Kanbas/Courses/1234/Home">
+                        Dog Grooming 106
+                    </a>
+                    <p className="wd-dashboard-course-title">
+                    How to Groom Your Pet at Home
+                    </p>
+                    <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+                    </div>
+                </div>
+                </td>
+                <td>
+                <div className="wd-dashboard-course"> 
+                <img src="/images/dog.jpg" width={200} />
+                <div>
+                    <a className="wd-dashboard-course-link"
+                        href="#/Kanbas/Courses/1234/Home">
+                        Dog Grooming 107
+                    </a>
+                    <p className="wd-dashboard-course-title">
+                    How to Groom Your Pet at Home
+                    </p>
+                    <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+                    </div>
+                </div>
+                </td>
+                
+            </tr>
+        </table>
+         
+          
+         
+
+        
+
+        </div>
+      </div>
+  );}
+  
