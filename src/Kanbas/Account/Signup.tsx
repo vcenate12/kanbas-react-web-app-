39b8e20cdc1b7bfc5e2a93b145eea719c3e17c30
vcenate12 <156140634+vcenate12@users.noreply.github.com>
2024-10-04 10:14:56 -0400

    a2 css and bootstrap2

--- src/Kanbas/Account/Signup.tsx
+++ src/Kanbas/Account/Signup.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Signup() {
   return (
     <div id="wd-signup-screen">
-      <h3>Sign up</h3>
+      <h3>Sign up Today</h3>
       <input placeholder="username" className="wd-username form-control mb-2" />
       <input placeholder="password" type="password" className="wd-password form-control mb-2" />
       <input placeholder="verify password" type="password" className="wd-password-verify form-control mb-2" />
